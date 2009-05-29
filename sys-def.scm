825c0df5b220903c84cbeb98070e941fef2b7d26
Andreas Rottmann <a.rottmann@gmx.at>
2009-05-29 17:45:00 +0200

    Create data symlink in build script

--- sys-def.scm
+++ sys-def.scm
@@ -5,11 +5,25 @@
 
   (conjure
    (import (rnrs)
-           (conjure dsl)
-           (sbank support conjure))
+           (only (spells filesys)
+                 create-symbolic-link)
+           (spells pathname)
+           (conjure dsl))
 
    (task (configure
+          (depends 'data-symlink)
           (produce '((("sbank") "glib.sls") <= "glib.sls.in")
                    '((("sbank") "gtk.sls") <= "gtk.sls.in")
                    '((("sbank") "soup.sls") <= "soup.sls.in"))
-          (fetchers (typelib-fetcher))))))
+          (fetchers (procedure-from-environment/lazy
+                     (typelib-fetcher)
+                     (sbank support conjure)))))
+
+   (task data-symlink
+         (file
+          (product '(("sbank") "data"))
+          (proc (lambda (step)
+                  (create-symbolic-link
+                   (pathname-join '((back) () #f)
+                                  ((step 'project) 'source-dir) "data")
+                   '(("sbank") "data"))))))))
